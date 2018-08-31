b09998910c50f4058bf4f24e9455a400455051f9
SplayD027 <Top074Rock@Gmail.com>
2018-08-31 14:19:40 +0700

    added basic countdown function

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Pomodoro clock
 
 this is part of the Odin Project
+
+**ToDoList**
+- working clock **DONE**
+- loop
+- be able to set the timer
+- notification
+- visual indication of divied slice of time and progress
+  - etc. (O) (O) () ()
--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-document.getElementById("title").style.color = "blue";
+var minute, second, loop;
+minute = document.querySelector("#minute").textContent;
+second = document.querySelector("#second").textContent;
+loop = document.querySelector("#loop").textContent;
+ 
+// stolen from stackoverflow
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        document.querySelector("#minute").textContent = minutes;
+		document.querySelector("#second").textContent = seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var totalseconds = (Number(minute) * 60) + Number(second);
+    startTimer(totalseconds);
+};

24743b900c1ae408bfd82a0953dcdd755b486867
SplayD027 <Top074Rock@Gmail.com>
2018-08-31 15:21:36 +0700

    added loop function

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ this is part of the Odin Project
 **ToDoList**
 - working clock **DONE**
 - loop
+- start and pause which changed into restart button when paused
 - be able to set the timer
 - notification
 - visual indication of divied slice of time and progress
--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ var minute, second, loop;
 minute = document.querySelector("#minute").textContent;
 second = document.querySelector("#second").textContent;
 loop = document.querySelector("#loop").textContent;
- 
+var totalseconds = (Number(minute) * 60) + Number(second);
+
 // stolen from stackoverflow
 function startTimer(duration) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
+    var timer = duration, minutes, seconds, loops = Number(loop);
+    var countdown = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+        seconds = parseInt(timer % 60, 10);		
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -16,13 +17,18 @@ function startTimer(duration) {
         document.querySelector("#minute").textContent = minutes;
 		document.querySelector("#second").textContent = seconds;
 
-        if (--timer < 0) {
-            timer = duration;
+        if (timer-- == 0) {
+			if (loops-- == 0) {
+            	clearInterval(countdown);
+			}
+			else {
+				timer = duration;
+				document.querySelector("#loop").textContent = loops;
+			}
         }
     }, 1000);
 }
 
-window.onload = function () {
-    var totalseconds = (Number(minute) * 60) + Number(second);
+window.onload = function () {   
     startTimer(totalseconds);
 };

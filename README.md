0a800e610d99c8248db6b06656dd36cc9f3582b3
SplayD027 <Top074Rock@Gmail.com>
2018-08-31 21:29:21 +0700

    added toggle start/pause button and redid a lot of codes

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ this is part of the Odin Project
 
 **ToDoList**
 - working clock **DONE**
-- loop
-- start and pause which changed into restart button when paused
-- be able to set the timer
+- loops **DONE**
+- start/pause toggle button **DONE**
+- restart button when paused
+- break and working phase
+- be able to set the timer and loops
 - notification
 - visual indication of divied slice of time and progress
   - etc. (O) (O) () ()
--- js/script.js
+++ js/script.js
@@ -1,34 +1,74 @@
+//DELETE THIS LATER
+var document,setInterval,clearInterval,window;
+//global variables
 var minute, second, loop;
-minute = document.querySelector("#minute").textContent;
-second = document.querySelector("#second").textContent;
-loop = document.querySelector("#loop").textContent;
-var totalseconds = (Number(minute) * 60) + Number(second);
+minute = document.querySelector("#minute");
+second = document.querySelector("#second");
+loop = document.querySelector("#loop");
 
+var totalseconds, progress;
+totalseconds = (Number(minute.textContent) * 60) + Number(second.textContent);
+progress = totalseconds;
+
+var thebutton, restartbutton; 
+thebutton = document.querySelector("#thebutton");
+restartbutton = document.querySelector("#restart");
+
+var clock, clockstatus = 0;
+
+//functions
 // stolen from stackoverflow
-function startTimer(duration) {
-    var timer = duration, minutes, seconds, loops = Number(loop);
-    var countdown = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);		
+function countdown() {
+	var timer = progress, minutes, seconds, loops = Number(loop.textContent);
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);		
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        document.querySelector("#minute").textContent = minutes;
-		document.querySelector("#second").textContent = seconds;
-
-        if (timer-- == 0) {
-			if (loops-- == 0) {
-            	clearInterval(countdown);
-			}
-			else {
-				timer = duration;
-				document.querySelector("#loop").textContent = loops;
-			}
-        }
-    }, 1000);
+    minute.textContent = minutes;
+	second.textContent = seconds;
+	
+    if (timer-- == 0) {
+		if (loops-- == 0) {
+			clearInterval(clock);
+		}
+		else {
+			timer = totalseconds;
+			//if (loops == 0) {loop.textContent = ""}
+			//else loop.textContent = loops;
+			loop.textContent = loops;
+		}
+    }
+	progress = timer;
 }
 
-window.onload = function () {   
-    startTimer(totalseconds);
-};
+function clockcontrol() {
+	if (clockstatus == 0){
+		clockstatus = 1;
+		thebutton.textContent = "pause";
+		clock = setInterval(countdown, 1000);
+	}
+	else {
+		clockstatus = 0;
+		thebutton.textContent = "start";
+		clearInterval(clock);
+	}
+}
+
+function restart() {
+	minute.textContent = "25";
+	second.textContent = "00";
+	loop.textContent = "2";
+}
+
+function check() {
+	console.log("html:",minute.textContent,second.textContent,loop.textContent);
+	console.log("total:", totalseconds);
+	console.log("progress:", progress);
+	console.log("clockstatus:",clockstatus);
+}
+
+//event listeners
+thebutton.addEventListener("click", ()=>{clockcontrol()});
+restartbutton.addEventListener("click", ()=>{restart()});
